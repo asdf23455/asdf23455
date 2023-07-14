bc253480ea5d7a674f3f4f5cf86b0096340f0b17
asdf23455 <139219339+asdf23455@users.noreply.github.com>
2023-07-14 11:14:00 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function changeImage(src) {
+    document.querySelector("img").src = "../images/" + src ;
+}
+ 
+document.body.onmousedown = function () {
+    count += 1;
+     document.querySelector("#pop").textContent = count;   
+    changeImage("unko_gorilla.png");
+    changeImage("s0090_25_0.png");
+}
+document.body.onmouseup = function () {
+    count += 1;
+    document.querySelector("#pop").textContent = count;   
+    changeImage("gorilla_banana.png");
+}
